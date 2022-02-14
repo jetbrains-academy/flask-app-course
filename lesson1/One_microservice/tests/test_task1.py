14f83b1af840557252a8a9fa68a16d50bdcad5d6
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-02-14 20:23:46 +0300

    Minor changes in test 1

--- lesson1/One_microservice/tests/test_task1.py
+++ lesson1/One_microservice/tests/test_task1.py
@@ -6,16 +6,13 @@ import string
 
 class TestCase(unittest.TestCase):
     def test_post(self):
-        car_id = ''.join(random.choices(string.ascii_lowercase, k=10))
-        test_car = {'name': f'TestCar_{car_id}'}
-        response = requests.post('http://127.0.0.1:5000/books', json={'title': 'TestCar12345', 'author': 'Author', 'length': 234, 'rating': 0.9})
+        book_id = ''.join(random.choices(string.ascii_lowercase, k=10))
+        response = requests.post('http://127.0.0.1:5000/books', json={'title': f'TestBook_{book_id}', 'author': 'Author', 'length': 234, 'rating': 0.9})
         print(response.text)
         self.assertEqual(response.status_code, 201, msg="Such a bad day :(")
 
     def test_add(self):
         response = requests.get('http://127.0.0.1:5000/books')
-        # url = response.url
-        # status_code = response.status_code
         print(response.url)
         print(response.status_code)
         print(response.text)
