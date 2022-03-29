b2f2ab61040aa0fffb6eed81f41c62db77c62de0
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-03-29 21:34:06 +0200

    Created mock API with Device resource class and a dictionary

--- Building the API/API - Device Resource/api.py
+++ Building the API/API - Device Resource/api.py
@@ -1,10 +1,29 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
-import dal
 
 # Initialize Flask
 app = Flask(__name__)
 api = Api(app)
+
+devices = [{
+    "id": "001",
+    "name": "Light bulb",
+    "location": "hall",
+    "status": "off"
+},
+    {
+        "id": "002",
+        "name": "Humidity_sensor",
+        "location": "bedroom",
+        "status": "on"
+    },
+    {
+        "id": "003",
+        "name": "Humidifier",
+        "location": "bedroom",
+        "status": "off"
+    }
+]
 
 
 # Resource: Individual Device Routes
@@ -20,8 +39,10 @@ class Device(Resource):
         super(Device, self).__init__()
 
     # GET - Returns a single device object given a matching id
+    @staticmethod
     def get(self, identifier):
-        device = dal.get_device(identifier)
+        # device = dal.get_device(identifier)
+        device = next((item for item in devices if item['id'] == identifier), None)
 
         if not device:
             return {'message': 'Device not found', 'data': {}}, 404
@@ -31,19 +52,33 @@ class Device(Resource):
     # PUT - Given an id
     def put(self, identifier):
         args = self.reqparse.parse_args()
-        updated_device = dal.put_device(identifier, args)
-
-        if not updated_device:
+        # updated_device = dal.put_device(identifier, args)
+        for device in devices:
+            if device.get('id', 0) == identifier:
+                for k, v in args.items():
+                    if v is not None:
+                        device[k] = v
+                break
+        else:
+            # if not updated_device:
             return {'message': 'Device not found', 'data': {}}, 404
 
-        return {"updated device": updated_device}
+        # return {"updated device": updated_device}
+        return {"updated device": next((item for item in devices if item['id'] == identifier), None)}
 
     # Delete - Given an id
     @staticmethod
     def delete(identifier):
-        deleted = dal.delete_device(identifier)
+        # deleted = dal.delete_device(identifier)
+        #
+        # if not deleted:
+        #     return {'message': 'Device not found', 'data': {}}, 404
 
-        if not deleted:
+        for i in range(len(devices)):
+            if devices[i]['id'] == identifier:
+                del devices[i]
+                break
+        else:
             return {'message': 'Device not found', 'data': {}}, 404
 
-        return deleted, 201
+        return {'message': f'{identifier} deleted'}, 201
