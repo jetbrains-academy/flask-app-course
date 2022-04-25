0dcf800467cb43a29ab13defa4d47506ab06a8dd
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-04-25 22:52:38 +0200

    Added comments to dal.py in crud operations on db 2 task, added text

--- Building the API/CRUD operations on DB 2/dal.py
+++ Building the API/CRUD operations on DB 2/dal.py
@@ -2,6 +2,8 @@ import shelve
 from flask import g
 
 
+# This function creates a database if none has yet been created,
+# or opens it if it's already there.
 def pull_db():
     db_ = getattr(g, '_database', None)
     if db_ is None:
@@ -9,21 +11,24 @@ def pull_db():
     return db_
 
 
+# This function returns the entire dataset of devices as a dictionary
 def get():
     shelf = pull_db()
     keys = list(shelf.keys())
-    devices_ = []
+    devices_ = {}
     for key in keys:
-        devices_.append(shelf[key])
+        devices_[key] = shelf[key]
     return devices_
 
 
+# This function adds a new element to the datastore of devices
 def post(args):
     shelf = pull_db()
     shelf[args['id']] = args
     return shelf[args['id']]
 
 
+# This function retrieves an item by its identifier and returns it
 def get_device(identifier):
     shelf = pull_db()
 
@@ -32,6 +37,8 @@ def get_device(identifier):
     return shelf[identifier]
 
 
+# This function should update an item in the database, access the item by its id.
+# Return None if the identifier is not found.
 def put_device(identifier, args):
     shelf = pull_db()
     if not (identifier in shelf):
@@ -43,10 +50,13 @@ def put_device(identifier, args):
         if v is not None:
             # if not, set the element in the devices dict with the 'k' object to the value provided in the request.
             device[k] = v
+    # Re-assign the new value of device to the identifier in shelve to save changes.
     shelf[identifier] = device
     return shelf[identifier]
 
 
+# This function should delete an item by its identifier.
+# Return None if the identifier is not found.
 def delete_device(identifier):
     shelf = pull_db()
     # if the key does not exist on the shelf, return a 404 error.
