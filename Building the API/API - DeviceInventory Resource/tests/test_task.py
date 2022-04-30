bd3949bd56d90d605e4348334b50c1c6c5bac11b
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-04-30 19:13:11 +0200

    Removed comments from DeviceInventory Resource tests, removed committed db files from tasks

--- Building the API/API - DeviceInventory Resource/tests/test_task.py
+++ Building the API/API - DeviceInventory Resource/tests/test_task.py
@@ -1,16 +1,3 @@
-# import unittest
-# import requests
-#
-#
-# # todo: replace this with an actual test
-# class TestCase(unittest.TestCase):
-#     def test_get(self):
-#         response = requests.get('http://0.0.0.0:5000/items')
-#         print(response.status_code)
-#         print(response.text)
-#         self.assertEqual(response.status_code, 200, msg=f"GET request resulted in an unexpected response code: {response.status_code}")
-
-
 import flask_unittest
 
 from api import app
