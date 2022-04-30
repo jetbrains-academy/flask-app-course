bba5117c26a49616e4f4e5cf5790010fa320a772
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-04-30 19:12:21 +0200

    Added tests to Modify API

--- Building the API/Modify API/tests/test_task.py
+++ Building the API/Modify API/tests/test_task.py
@@ -1,9 +1,31 @@
-import unittest
+import flask_unittest
 
-from task import sum
+from api import app
 
 
-# todo: replace this with an actual test
-class TestCase(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(sum(1, 2), 3, msg="adds 1 + 2 to equal 3")
+class TestClient(flask_unittest.ClientTestCase):
+    # Assign the flask app object
+    app = app
+
+    def test_get_with_client(self, client):
+        response = client.get('/items')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_with_client(self, client):
+        response = client.post('/items', json={"id": "100500", "name": "TestDevice", "location": "somewhere", "status": "off"})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data, b'{"device": {"id": "100500", "name": "TestDevice", "location": "somewhere", '
+                                        b'"status": "off"}}\n')
+
+    def test_get__with_client(self, client):
+        response = client.get('/items/001')
+        self.assertEqual(response.data, b'{"device": {"id": "001", "name": "Light bulb", "location": "hall", '
+                                        b'"status": "off"}}\n')
+
+    def test_put_with_client(self, client):
+        response = client.put('/items/003', data={'location': 'hall', 'status': 'off'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_with_client(self, client):
+        response = client.delete('/items/002')
+        self.assertEqual(response.status_code, 201)
