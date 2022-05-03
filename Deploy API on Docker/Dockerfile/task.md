4d4bdfae3f2ad97b80d44e0b4ac86d1d54f92507
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-05-03 18:51:20 +0200

    Added text, tests, configuration files to Dockerfile task

--- Deploy API on Docker/Dockerfile/task.md
+++ Deploy API on Docker/Dockerfile/task.md
@@ -1,1 +1,36 @@
-Dockerfile explanation, requirements.txt
+## requirements.txt and Dockerfile
+
+### requirements.txt
+As we discussed earlier, the list of packages needed for our application can be placed in a `requirements.txt` file,
+which can then be committed to version control and shipped as part of an application.
+Users can then install all the necessary packages with `pip install -r requirements.txt`. In our case, Docker will be installing the packages.
+At this stage, you need to add `Flask` and `flask-restful` to the file.
+
+### Dockerfile
+
+In order to deploy this microservice API to a docker container we will first need to create a Dockerfile in the project directory.
+A Dockerfile is in essence a set of instructions for building an Image which is a blueprint for our container to run off of.
+Lets go over this line by line.
+
+```text
+FROM python:3.8
+COPY requirements.txt /
+RUN pip install -r /requirements.txt
+COPY . /app
+WORKDIR /app
+EXPOSE 5000
+CMD [ "python", "api.py" ]
+```
+
+The line `FROM python:3.8` uses the python 3.8 image from Docker Hub as our base image, This makes sure we have Python 
+and all of its dependencies on the container
+We `COPY requirements.txt /` which makes the file available in our docker image, and then install it using 
+`RUN pip install -r /requirements.txt`. We then copy the rest of our source code into a subdirectory called app with 
+`COPY . /app`. Using `WORKDIR /app` means the next commands will be executed in that directory. We `EXPOSE 5000` to expose 
+port 5000, which is the port defined in our `application.py`. Then finally we state the command to run the application, 
+which is `CMD [ "python", "application.py" ]`.
+
+Add these lines to the empty Dockerfile that already exists in the project.
+
+To run your app and see if it works, press Check.
+
--- Deploy API on Docker/Dockerfile/tests/test_task.py
+++ Deploy API on Docker/Dockerfile/tests/test_task.py
@@ -1,9 +1,40 @@
 import unittest
+import requests
+import random
+import string
+import time
 
-from task import sum
+time.sleep(3)
 
-
-# todo: replace this with an actual test
 class TestCase(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(sum(1, 2), 3, msg="adds 1 + 2 to equal 3")
+    def test_post(self):
+        device_id = ''.join(random.choices(string.ascii_lowercase, k=10))
+        response = requests.post('http://0.0.0.0:5000/items', json={"id": f"{device_id}",
+                                                                      "name": "UPDATEDUPDATEDUPDATED",
+                                                                      "location": "location",
+                                                                      "status": "off"})
+        print(response.status_code)
+        print(response.text)
+        self.assertEqual(response.status_code, 201, msg="Such a bad day :(")
+
+    def test_get(self):
+        response = requests.get('http://0.0.0.0:5000/items')
+        print(response.status_code)
+        print(response.text)
+        self.assertEqual(response.status_code, 200, msg=f"GET request resulted in an unexpected response code: {response.status_code}")
+
+    def test_put(self):
+        # device_id = ''.join(random.choices(string.ascii_lowercase, k=10))
+        response = requests.put('http://0.0.0.0:5000/items/002', json={"id": "002",
+                                                                         "name": "Humidity_sensor",
+                                                                         "location": "bedroom",
+                                                                         "status": "off"})
+        print(response.status_code)
+        print(response.text)
+        self.assertEqual(response.status_code, 200, msg=f"PUT request resulted in an unexpected response code: {response.status_code}")
+
+    def test_delete(self):
+        response = requests.delete('http://0.0.0.0:5000/items/100')
+        print(response.status_code)
+        print(response.text)
+        self.assertEqual(response.status_code, 404, msg=f"DELETE request resulted in an unexpected response code: {response.status_code}")
