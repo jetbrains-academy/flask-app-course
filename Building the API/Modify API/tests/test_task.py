7441565e31a64fc92b8b8ee1368f93cff31f8815
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-07-04 21:41:11 +0200

    refactor(Modify API task):
    Extracted error message into the not_found_response attribute,
    constructed it as a Response object, added more tests

--- Building the API/Modify API/tests/test_task.py
+++ Building the API/Modify API/tests/test_task.py
@@ -10,22 +10,48 @@ class TestClient(flask_unittest.ClientTestCase):
     def test_get_with_client(self, client):
         response = client.get('/items')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, (b'{"items":{"001":{"id":"001","location":"hall","name":"Light bulb","status":"'
+                                         b'off"},"003":{"id":"003","location":"bedroom","name":"Humidifier","status":"o'
+                                         b'ff"},"100500":{"id":"100500","location":"somewhere","name":"TestDevice","sta'
+                                         b'tus":"off"}}}\n'))
 
     def test_post_with_client(self, client):
         response = client.post('/items', json={"id": "100500", "name": "TestDevice", "location": "somewhere", "status": "off"})
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data, b'{"device": {"id": "100500", "name": "TestDevice", "location": "somewhere", '
-                                        b'"status": "off"}}\n')
+        self.assertEqual(response.data, (b'{"device":{"id":"100500","location":"somewhere","name":"TestDevice","status"'
+                                         b':"off"}}\n'))
 
     def test_get__with_client(self, client):
         response = client.get('/items/001')
-        self.assertEqual(response.data, b'{"device": {"id": "001", "name": "Light bulb", "location": "hall", '
-                                        b'"status": "off"}}\n')
+        self.assertEqual(response.data, (b'{"device":{"id":"001","location":"hall","name":"Light bulb","status":"off"}}'
+                                         b'\n'))
 
     def test_put_with_client(self, client):
         response = client.put('/items/003', data={'location': 'hall', 'status': 'off'})
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, (b'{"updated device":{"id":"003","location":"bedroom","name":"Humidifier","stat'
+                                         b'us":"off"}}\n'))
 
     def test_delete_with_client(self, client):
         response = client.delete('/items/002')
         self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data, b'{"deleted device":"002"}\n')
+
+    def test_delete_with_client_error(self, client):
+        response = client.delete('/items/002222')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data, b'{"data":{},"message":"Device not found"}\n')
+
+    def test_put_with_client_error(self, client):
+        response = client.put('/items/003333', data={'location': 'hall', 'status': 'off'})
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data, b'{"data":{},"message":"Device not found"}\n')
+
+    def test_get_with_client_error(self, client):
+        response = client.get('/items/001111')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data, b'{"data":{},"message":"Device not found"}\n')
+
+    def test_post_with_client_error(self, client):
+        response = client.post('/', json={"id": "100500", "name": "TestDevice", "location": "somewhere", "status": "off"})
+        self.assertEqual(response.status_code, 404)
