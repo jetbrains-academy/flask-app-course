a601ccfcbb5d4babceb424aedc460353566d198c
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-07-08 16:19:15 +0200

    Updated the test system in Get Resource (Works now)

--- Building the Gateway Application/Get Resource/gateway/application.py
+++ Building the Gateway Application/Get Resource/gateway/application.py
@@ -14,9 +14,9 @@ def index():
 def get_resource(item_id=None):
     # Forward the request to the relevant endpoint in invsys
     if item_id:
-        response = requests.get(f'http://0.0.0.0:5000/items/{item_id}')
+        response = requests.get(f'http://invsys:5000/items/{item_id}')
     else:
-        response = requests.get('http://0.0.0.0:5000/items')
+        response = requests.get('http://invsys:5000/items')
 
     # Forward the response back to the client
     # We create a Response object by deconstructing our response from above
