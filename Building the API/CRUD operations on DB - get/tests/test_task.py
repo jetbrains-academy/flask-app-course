dc77ad74ece824ad30d3aec3677c3137cbdebcf6
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-09-20 17:57:19 +0200

    Fixed some test issues in CRUD operations on DB - get

--- Building the API/CRUD operations on DB - get/tests/test_task.py
+++ Building the API/CRUD operations on DB - get/tests/test_task.py
@@ -7,11 +7,14 @@ class TestClient(flask_unittest.ClientTestCase):
     # Assign the flask app object
     app = app
 
-    def test_get_with_client(self, client):
+    def test_get_with_client_01(self, client):
         response = client.get('/items')
-        self.assertEqual(response.status_code, 200, msg=f"GET request resulted in an unexpected response code {response.status_code}.")
-        self.assertEqual(response.data, (b'{"004": {"id": "004", "name": "TestDevice", "location": "somewhere", "status'
+        self.assertEqual(200, response.status_code, msg=f"GET request resulted in an unexpected response code {response.status_code}.")
+
+    def test_get_with_client_02(self, client):
+        response = client.get('/items')
+        self.assertEqual((b'{"004": {"id": "004", "name": "TestDevice", "location": "somewhere", "status'
                                          b'": "off"}, "002": {"id": "002", "name": "Humidity_sensor", "location": "bedr'
                                          b'oom", "status": "on"}, "001": {"id": "001", "name": "Light bulb", "location"'
                                          b': "hall", "status": "off"}, "003": {"id": "003", "name": "Humidifier", "loca'
-                                         b'tion": "bedroom", "status": "off"}}\n'), msg="GET request resulted in unexpected response content.")
+                                         b'tion": "bedroom", "status": "off"}}\n'), response.data, msg="GET request resulted in unexpected response content.")
