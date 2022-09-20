6477a49d151660c0864e64aee7a98e10449d3eb6
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-09-20 18:05:41 +0200

    Fixed some test issues in Modify API

--- Building the API/Modify API/tests/test_task.py
+++ Building the API/Modify API/tests/test_task.py
@@ -9,46 +9,46 @@ class TestClient(flask_unittest.ClientTestCase):
 
     def test_get_with_client(self, client):
         response = client.get('/items')
-        self.assertEqual(response.status_code, 200, msg=f"GET request resulted in an unexpected response code {response.status_code}.")
+        self.assertEqual(200, response.status_code, msg=f"GET request resulted in an unexpected response code {response.status_code}.")
 
     def test_post_with_client(self, client):
         response = client.post('/items', json={"id": "100500", "name": "TestDevice", "location": "somewhere", "status": "off"})
-        self.assertEqual(response.status_code, 201, msg=f"POST request resulted in an unexpected response code {response.status_code}.")
-        self.assertEqual(response.data, (b'{"device":{"id":"100500","location":"somewhere","name":"TestDevice","status"'
-                                         b':"off"}}\n'), msg="POST request resulted in unexpected response content.")
+        self.assertEqual(201, response.status_code, msg=f"POST request resulted in an unexpected response code {response.status_code}.")
+        self.assertEqual((b'{"device":{"id":"100500","location":"somewhere","name":"TestDevice","status"'
+                                         b':"off"}}\n'), response.data, msg="POST request resulted in unexpected response content.")
 
     def test_get_id_with_client(self, client):
         response = client.get('/items/001')
-        self.assertEqual(response.status_code, 200, msg=f"GET request resulted in an unexpected response code {response.status_code}.")
-        self.assertEqual(response.data, (b'{"device":{"id":"001","location":"hall","name":"Light bulb","status":"off"}}'
-                                         b'\n'), msg="GET request resulted in unexpected response content.")
+        self.assertEqual(200, response.status_code, msg=f"GET request resulted in an unexpected response code {response.status_code}.")
+        self.assertEqual((b'{"device":{"id":"001","location":"hall","name":"Light bulb","status":"off"}}'
+                                         b'\n'), response.data, msg="GET request resulted in unexpected response content.")
 
     def test_put_with_client(self, client):
-        response = client.put('/items/003', data={'location': 'hall', 'status': 'off'})
-        self.assertEqual(response.status_code, 200, msg=f"PUT request resulted in an unexpected response code {response.status_code}.")
-        self.assertEqual(response.data, (b'{"updated device":{"id":"003","location":"bedroom","name":"Humidifier","stat'
-                                         b'us":"off"}}\n'), msg="PUT request resulted in unexpected response content.")
+        response = client.put('/items/003', json={'location': 'hall', 'status': 'off'})
+        self.assertEqual(200, response.status_code, msg=f"PUT request resulted in an unexpected response code {response.status_code}.")
+        self.assertEqual((b'{"updated device":{"id":"003","location":"hall","name":"Humidifier","stat'
+                                         b'us":"off"}}\n'), response.data, msg="PUT request resulted in unexpected response content.")
 
     def test_delete_with_client(self, client):
         response = client.delete('/items/002')
-        self.assertEqual(response.status_code, 201, msg=f"DELETE request resulted in an unexpected response code {response.status_code}.")
-        self.assertEqual(response.data, b'{"deleted device":"002"}\n', msg="DELETE request resulted in unexpected response content.")
+        self.assertEqual(201, response.status_code, msg=f"DELETE request resulted in an unexpected response code {response.status_code}.")
+        self.assertEqual(b'{"deleted device":"002"}\n', response.data, msg="DELETE request resulted in unexpected response content.")
 
     def test_delete_with_client_error(self, client):
         response = client.delete('/items/002222')
-        self.assertEqual(response.status_code, 404, msg=f"DELETE request resulted in an unexpected response code {response.status_code}.")
-        self.assertEqual(response.data, b'{"data":{},"message":"Device not found"}\n', msg="DELETE request resulted in unexpected response content.")
+        self.assertEqual(404, response.status_code, msg=f"DELETE request resulted in an unexpected response code {response.status_code}.")
+        self.assertEqual(b'{"data":{},"message":"Device not found"}\n', response.data, msg="DELETE request resulted in unexpected response content.")
 
     def test_put_with_client_error(self, client):
-        response = client.put('/items/003333', data={'location': 'hall', 'status': 'off'})
-        self.assertEqual(response.status_code, 404, msg=f"PUT request resulted in an unexpected response code {response.status_code}.")
-        self.assertEqual(response.data, b'{"data":{},"message":"Device not found"}\n', msg="PUT request resulted in unexpected response content.")
+        response = client.put('/items/003333', json={'location': 'hall', 'status': 'off'})
+        self.assertEqual(404, response.status_code, msg=f"PUT request resulted in an unexpected response code {response.status_code}.")
+        self.assertEqual(b'{"data":{},"message":"Device not found"}\n', response.data, msg="PUT request resulted in unexpected response content.")
 
     def test_get_with_client_error(self, client):
         response = client.get('/items/001111')
-        self.assertEqual(response.status_code, 404, msg=f"GET request resulted in an unexpected response code {response.status_code}.")
-        self.assertEqual(response.data, b'{"data":{},"message":"Device not found"}\n', msg="GET request resulted in unexpected response content.")
+        self.assertEqual(404, response.status_code, msg=f"GET request resulted in an unexpected response code {response.status_code}.")
+        self.assertEqual(b'{"data":{},"message":"Device not found"}\n', response.data, msg="GET request resulted in unexpected response content.")
 
     def test_post_with_client_error(self, client):
         response = client.post('/', json={"id": "100500", "name": "TestDevice", "location": "somewhere", "status": "off"})
-        self.assertEqual(response.status_code, 404, msg=f"POST request resulted in an unexpected response code {response.status_code}.")
+        self.assertEqual(404, response.status_code, msg=f"POST request resulted in an unexpected response code {response.status_code}.")
