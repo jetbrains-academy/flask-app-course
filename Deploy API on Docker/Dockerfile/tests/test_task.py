c872c71263dcae0502b20667598244323d0fdf1c
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-09-28 13:10:27 +0200

    fix(Dockerfile tests): updated the addresses to avoid errors in testing on other OSs

--- Deploy API on Docker/Dockerfile/tests/test_task.py
+++ Deploy API on Docker/Dockerfile/tests/test_task.py
@@ -9,7 +9,7 @@ time.sleep(3)
 class TestCase(unittest.TestCase):
     def test_post(self):
         device_id = ''.join(random.choices(string.ascii_lowercase, k=10))
-        response = requests.post('http://0.0.0.0:5000/items', json={"id": f"{device_id}",
+        response = requests.post('http://127.0.0.1:5000/items', json={"id": f"{device_id}",
                                                                       "name": "UPDATEDUPDATEDUPDATED",
                                                                       "location": "location",
                                                                       "status": "off"})
@@ -18,14 +18,14 @@ class TestCase(unittest.TestCase):
         self.assertEqual(201, response.status_code, msg="Such a bad day :(")
 
     def test_get(self):
-        response = requests.get('http://0.0.0.0:5000/items')
+        response = requests.get('http://127.0.0.1:5000/items')
         print(response.status_code)
         print(response.text)
         self.assertEqual(200, response.status_code, msg=f"GET request resulted in an unexpected response code: {response.status_code}")
 
     def test_put(self):
         # device_id = ''.join(random.choices(string.ascii_lowercase, k=10))
-        response = requests.put('http://0.0.0.0:5000/items/002', json={"id": "002",
+        response = requests.put('http://127.0.0.1:5000/items/002', json={"id": "002",
                                                                          "name": "Humidity_sensor",
                                                                          "location": "bedroom",
                                                                          "status": "off"})
@@ -34,7 +34,7 @@ class TestCase(unittest.TestCase):
         self.assertEqual(200, response.status_code, msg=f"PUT request resulted in an unexpected response code: {response.status_code}")
 
     def test_delete(self):
-        response = requests.delete('http://0.0.0.0:5000/items/100')
+        response = requests.delete('http://127.0.0.1:5000/items/100')
         print(response.status_code)
         print(response.text)
         self.assertEqual(404, response.status_code, msg=f"DELETE request resulted in an unexpected response code: {response.status_code}")
