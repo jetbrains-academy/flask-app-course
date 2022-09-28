fef099c84d851e8e28c3fbde53511facae5c90ef
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-09-28 16:34:05 +0200

    Fixed tests, added configurations to "Add a Dockerfile and requirements" to remove previous versions of the containers before launching tests

--- Building the Gateway Application/Add a Dockerfile and requirements.txt/tests/test_task.py
+++ Building the Gateway Application/Add a Dockerfile and requirements.txt/tests/test_task.py
@@ -1,9 +1,22 @@
 import unittest
+import requests
+import time
 
-from task import sum
+time.sleep(3)
 
 
-# todo: replace this with an actual test
 class TestCase(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(sum(1, 2), 3, msg="adds 1 + 2 to equal 3")
+    def test_get(self):
+        response = requests.get('http://127.0.0.1:5001/items')
+        print(response.status_code)
+        print(response.text)
+        self.assertEqual(200, response.status_code,
+                         msg=f"GET request resulted in an unexpected response code: {response.status_code}")
+        self.assertEqual((b'{\n  "items": {\n    "001": {\n      "id": "001",\n      "location": "hall",'
+                          b'\n      "name": "Light bulb",\n      "status": "off"\n    },\n    "002":'
+                          b' {\n      "id": "002",\n      "location": "bedroom",\n      "name": "Humidi'
+                          b'ty_sensor",\n      "status": "on"\n    },\n    "003": {\n      "id": "00'
+                          b'3",\n      "location": "bedroom",\n      "name": "Humidifier",\n      "stat'
+                          b'us": "off"\n    }\n  }\n}\n').decode("utf-8").replace(" ", ""),
+                         response.content.decode("utf-8").replace(" ", ""),
+                         msg="GET request resulted in unexpected response content.")
