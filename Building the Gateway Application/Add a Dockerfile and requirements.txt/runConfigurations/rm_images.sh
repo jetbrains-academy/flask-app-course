24b33b2ecf9ec37d526cce2a07651962c3ef83ff
sofiiako <sofiia.kolchanova@gmail.com>
2023-05-04 21:31:56 +0200

    Updated tests and config files in "Add Dockerfile and requirements" for the new test infrastructure

--- Building the Gateway Application/Add a Dockerfile and requirements.txt/runConfigurations/rm_images.sh
+++
@@ -1,8 +0,0 @@
-if docker container ls | grep -q "flask-app-"; then
-  docker stop flask-app-gateway
-  docker rm flask-app-gateway
-  docker rmi flask-app-gateway-img
-  docker stop flask-app-invsys
-  docker rm flask-app-invsys
-  docker rmi flask-app-invsys-img
-fi
--- Building the Gateway Application/Add a Dockerfile and requirements.txt/tests/test_task.py
+++ Building the Gateway Application/Add a Dockerfile and requirements.txt/tests/test_task.py
@@ -1,22 +1,72 @@
 import unittest
 import requests
-import time
-
-time.sleep(3)
+import docker
 
 
-class TestCase(unittest.TestCase):
-    def test_get(self):
-        response = requests.get('http://127.0.0.1:5001/items')
-        print(response.status_code)
-        print(response.text)
-        self.assertEqual(200, response.status_code,
-                         msg=f"GET request resulted in an unexpected response code: {response.status_code}")
-        self.assertEqual((b'{\n  "items": {\n    "001": {\n      "id": "001",\n      "location": "hall",'
-                          b'\n      "name": "Light bulb",\n      "status": "off"\n    },\n    "002":'
-                          b' {\n      "id": "002",\n      "location": "bedroom",\n      "name": "Humidi'
-                          b'ty_sensor",\n      "status": "on"\n    },\n    "003": {\n      "id": "00'
-                          b'3",\n      "location": "bedroom",\n      "name": "Humidifier",\n      "stat'
-                          b'us": "off"\n    }\n  }\n}\n').decode("utf-8").replace(" ", ""),
-                         response.content.decode("utf-8").replace(" ", ""),
-                         msg="GET request resulted in unexpected response content.")
+client = docker.from_env()
+
+
+class TestSuiteWithAsyncTeardown(unittest.IsolatedAsyncioTestCase):
+    containers = []
+    completed_tests = 0
+    # TODO Set the correct number of tests here
+    total_tests = 1
+
+    async def async_setUp(self):
+        # TODO investigate hardcoded values for names and ports
+        self.container_names = ["flask-app-invsys", "flask-app-gateway"]
+        self.image_names = ["flask-app-invsys-img", "flask-app-gateway-img"]
+        self.container_ports = [None, 5001]
+        self.host_ports = [None, 5001]
+
+        TestSuiteWithAsyncTeardown.containers = []
+
+        for idx, container_name in enumerate(self.container_names):
+            container_list = client.containers.list(filters={'name': container_name})
+            if container_list:
+                container = container_list[0]
+                if container.status != 'running':
+                    container.restart()
+            else:
+                if self.container_ports[idx] is not None:
+                    container = client.containers.run(self.image_names[idx], name=container_name,
+                                                      ports={f'{self.container_ports[idx]}/tcp': self.host_ports[idx]},
+                                                      detach=True)
+                else:
+                    container = client.containers.run(self.image_names[idx], name=container_name, detach=True)
+            TestSuiteWithAsyncTeardown.containers.append(container)
+
+    async def async_tearDown(self):
+        TestSuiteWithAsyncTeardown.completed_tests += 1
+        if TestSuiteWithAsyncTeardown.completed_tests == TestSuiteWithAsyncTeardown.total_tests:
+            for container in self.containers:
+                container.stop()
+                container.remove()
+            client.close()
+
+    async def test_get(self):
+        await self.async_setUp()
+        try:
+            response = requests.get('http://127.0.0.1:5001/items')
+            print(response.status_code)
+            print(response.text)
+            self.assertEqual(200, response.status_code,
+                             msg=f"GET request resulted in an unexpected response code: {response.status_code}")
+            self.assertEqual((b'{\n  "items": {\n    "001": {\n      "id": "001",\n      "location": "hall",'
+                              b'\n      "name": "Light bulb",\n      "status": "off"\n    },\n    "002":'
+                              b' {\n      "id": "002",\n      "location": "bedroom",\n      "name": "Humidi'
+                              b'ty_sensor",\n      "status": "on"\n    },\n    "003": {\n      "id": "00'
+                              b'3",\n      "location": "bedroom",\n      "name": "Humidifier",\n      "stat'
+                              b'us": "off"\n    }\n  }\n}\n').decode("utf-8").replace(" ", ""),
+                             response.content.decode("utf-8").replace(" ", ""),
+                             msg="GET request resulted in unexpected response content.")
+        except Exception as e:
+            if isinstance(e, AssertionError):
+                await self.async_tearDown()
+                self.fail(msg=f"Unexpected response, {str(e)}")
+            else:
+                await self.async_tearDown()
+                self.fail(msg='Something went wrong. Try restarting Docker')
+        await self.async_tearDown()
+
+
