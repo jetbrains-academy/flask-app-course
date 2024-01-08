eca3001f11ddaadcc4f6e45c67b2232e96b1e1fa
Konstantin Chaika <konstantin.chaika@jetbrains.com>
2024-01-08 17:46:10 +0400

    New build proof of concept

--- Deploy API on Docker/Dockerfile/tests/test_task.py
+++ Deploy API on Docker/Dockerfile/tests/test_task.py
@@ -1,74 +1,38 @@
 import asyncio
+import subprocess
+import time
 import unittest
 import requests
 import docker
 import random
 import string
 
-
 client = docker.from_env()
 
 
 class TestSuiteWithAsyncTeardown(unittest.IsolatedAsyncioTestCase):
-    containers = []
-    completed_tests = 0
-    # TODO Set the correct number of tests here
-    total_tests = 4
+    healthcheck_url = 'http://127.0.0.1:5000/items'
 
-    async def async_setUp(self):
-        # TODO investigate hardcoded values for names and ports
-        self.container_names = ["flask-app-invsys"]
-        self.image_names = ["flask-app-invsys-img"]
-        self.container_ports = [None, 5001]
-        self.host_ports = [None, 5001]
+    @classmethod
+    def setUpClass(cls):
+        max_retries = 5
+        retry_interval = 1
+        for retry in range(max_retries):
+            try:
+                response = requests.get(cls.healthcheck_url)
+                response.raise_for_status()
+                break
+            except (requests.RequestException, requests.exceptions.HTTPError):
+                print('fail')
+                time.sleep(retry_interval)
+        else:
+            print(f"App on {cls.healthcheck_url} did not wake up after {max_retries} attempts.")
 
-        TestSuiteWithAsyncTeardown.containers = []
-
-        for idx, container_name in enumerate(self.container_names):
-            container_list = client.containers.list(filters={'name': container_name})
-            if container_list:
-                container = container_list[0]
-                if container.status != 'running':
-                    container.restart()
-            else:
-                if self.container_ports[idx] is not None:
-                    container = client.containers.run(self.image_names[idx], name=container_name,
-                                                      ports={f'{self.container_ports[idx]}/tcp': self.host_ports[idx]},
-                                                      detach=True)
-                else:
-                    container = client.containers.run(self.image_names[idx], name=container_name, detach=True)
-            TestSuiteWithAsyncTeardown.containers.append(container)
-
-    async def async_tearDown(self):
-        TestSuiteWithAsyncTeardown.completed_tests += 1
-        if TestSuiteWithAsyncTeardown.completed_tests == TestSuiteWithAsyncTeardown.total_tests:
-            for container in self.containers:
-                container.stop()
-                container.remove()
-            for image_name in self.image_names:
-                client.images.remove(image_name)
-            client.close()
-
-# teardown to remove ALL images:
-    # async def async_tearDown(self):
-    #     TestSuiteWithAsyncTeardown.completed_tests += 1
-    #     if TestSuiteWithAsyncTeardown.completed_tests == TestSuiteWithAsyncTeardown.total_tests:
-    #         client = docker.from_env()
-    #
-    #         # Stop and remove all containers
-    #         for container in client.containers.list(all=True):
-    #             container.stop()
-    #             container.remove(force=True)
-    #
-    #         # Remove all images
-    #         for image in client.images.list():
-    #             client.images.remove(image.id, force=True)
-    #
-    #         client.close()
-
+    @classmethod
+    def tearDownClass(cls):
+        subprocess.run(['docker', 'compose', 'down', '--volumes', '--rmi', 'all'], check=False)
 
     async def test_post(self):
-        await self.async_setUp()
         try:
             device_id = ''.join(random.choices(string.ascii_lowercase, k=10))
             response = requests.post('http://127.0.0.1:5000/items', json={"id": f"{device_id}",
@@ -77,36 +41,30 @@ class TestSuiteWithAsyncTeardown(unittest.IsolatedAsyncioTestCase):
                                                                           "status": "off"})
             print(response.status_code)
             print(response.text)
-            self.assertEqual(201, response.status_code, msg="Such a bad day :(")
+            self.assertEqual(201, response.status_code,
+                             msg=f"POST request resulted in an unexpected response code: {response.status_code}")
+
         except Exception as e:
             if type(e) == AssertionError:
-                await self.async_tearDown()
-                self.fail(msg=f"Unexpected response, {str(e)}")
+                self.fail(msg=f"Unexpected response for POST, {str(e)}")
             else:
-                await self.async_tearDown()
-                self.fail(msg='Something went wrong. Try restarting Docker')
-
-        await self.async_tearDown()
+                self.fail(msg=f'Something went wrong. Maybe your app is crashed: {str(e)}')
 
     async def test_get(self):
-        await self.async_setUp()
         try:
             response = requests.get('http://127.0.0.1:5000/items')
             print(response.status_code)
             print(response.text)
-            self.assertEqual(200, response.status_code, msg=f"GET request resulted in an unexpected response code: {response.status_code}")
+            self.assertEqual(200, response.status_code,
+                             msg=f"GET request resulted in an unexpected response code: {response.status_code}")
 
         except Exception as e:
             if isinstance(e, AssertionError):
-                await self.async_tearDown()
-                self.fail(msg=f"Unexpected response, {str(e)}")
+                self.fail(msg=f"Unexpected response for GET, {str(e)}")
             else:
-                await self.async_tearDown()
-                self.fail(msg='Something went wrong. Try restarting Docker')
-        await self.async_tearDown()
+                self.fail(msg=f'Something went wrong. Maybe your app is crashed: {str(e)}')
 
     async def test_put(self):
-        await self.async_setUp()
         try:
             # device_id = ''.join(random.choices(string.ascii_lowercase, k=10))
             response = requests.put('http://127.0.0.1:5000/items/002', json={"id": "002",
@@ -115,30 +73,25 @@ class TestSuiteWithAsyncTeardown(unittest.IsolatedAsyncioTestCase):
                                                                              "status": "off"})
             print(response.status_code)
             print(response.text)
-            self.assertEqual(200, response.status_code, msg=f"PUT request resulted in an unexpected response code: {response.status_code}")
+            self.assertEqual(200, response.status_code,
+                             msg=f"PUT request resulted in an unexpected response code: {response.status_code}")
 
         except Exception as e:
             if isinstance(e, AssertionError):
-                await self.async_tearDown()
-                self.fail(msg=f"Unexpected response, {str(e)}")
+                self.fail(msg=f"Unexpected response for PUT, {str(e)}")
             else:
-                await self.async_tearDown()
-                self.fail(msg='Something went wrong. Try restarting Docker')
-        await self.async_tearDown()
+                self.fail(msg=f'Something went wrong. Maybe your app is crashed: {str(e)}')
 
     async def test_delete(self):
-        await self.async_setUp()
         try:
             response = requests.delete('http://127.0.0.1:5000/items/100')
             print(response.status_code)
             print(response.text)
-            self.assertEqual(404, response.status_code, msg=f"DELETE request resulted in an unexpected response code: {response.status_code}")
+            self.assertEqual(404, response.status_code,
+                             msg=f"DELETE request resulted in an unexpected response code: {response.status_code}")
 
         except Exception as e:
             if isinstance(e, AssertionError):
-                await self.async_tearDown()
-                self.fail(msg=f"Unexpected response, {str(e)}")
+                self.fail(msg=f"Unexpected response for DELETE, {str(e)}")
             else:
-                await self.async_tearDown()
-                self.fail(msg='Something went wrong. Try restarting Docker')
-        await self.async_tearDown()
+                self.fail(msg=f'Something went wrong. Maybe your app is crashed: {str(e)}')
