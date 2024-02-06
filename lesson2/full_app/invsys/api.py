d2233dbb65883990fe333a8f12b9666f70262177
sofiiako <sofiia.kolchanova@gmail.com>
2024-02-06 19:51:00 +0100

    updated marshmallow schema in the full app

--- lesson2/full_app/invsys/api.py
+++ lesson2/full_app/invsys/api.py
@@ -1,41 +1,60 @@
 from flask import Flask, request, jsonify
-from marshmallow import Schema, fields, ValidationError, EXCLUDE
+from marshmallow import Schema, fields, ValidationError, EXCLUDE, validates_schema
 import dal
 
 app = Flask(__name__)
 
 
-# Define Marshmallow Schemas for request and response validation
+# Define Marshmallow Schema for request and response validation
 class DeviceSchema(Schema):
-    id = fields.Str()
-    name = fields.Str()
-    location = fields.Str()
-    status = fields.Str()
-
-
-# For POST requests where all fields are required
-class DevicePostSchema(DeviceSchema):
     id = fields.Str(required=True)
     name = fields.Str(required=True)
     location = fields.Str(required=True)
     status = fields.Str(required=True)
 
+    #Use validates_schema for custom validation logic
+    @validates_schema(pass_original=True)
+    def validate_required_fields(self, data, original_data, **kwargs):
+        required_fields = ['id', 'name', 'location', 'status']
+        if request.method == 'POST':
+            for field in required_fields:
+                if field not in original_data:
+                    raise ValidationError(f'{field} is required.', field)
 
-# For PUT requests where NOT all fields are required
-class DeviceUpdateSchema(Schema):
-    name = fields.Str(required=False)
-    location = fields.Str(required=False)
-    status = fields.Str(required=False)
 
-    class Meta:
-        unknown = EXCLUDE
+
+# # Define Marshmallow Schemas for request and response validation
+# class DeviceSchema(Schema):
+#     id = fields.Str()
+#     name = fields.Str()
+#     location = fields.Str()
+#     status = fields.Str()
+#
+#
+# # For POST requests where all fields are required
+# class DevicePostSchema(DeviceSchema):
+#     id = fields.Str(required=True)
+#     name = fields.Str(required=True)
+#     location = fields.Str(required=True)
+#     status = fields.Str(required=True)
+#
+#
+# # For PUT requests where NOT all fields are required
+# class DeviceUpdateSchema(Schema):
+#     name = fields.Str(required=False)
+#     location = fields.Str(required=False)
+#     status = fields.Str(required=False)
+#
+#     class Meta:
+#         unknown = EXCLUDE
+
 
 
 # Instantiate the schemas
 device_schema = DeviceSchema()
 devices_schema = DeviceSchema(many=True)  # For multiple devices
-device_post_schema = DevicePostSchema()
-device_update_schema = DeviceUpdateSchema()
+# device_post_schema = DeviceSchema()
+# device_update_schema = DeviceSchema()
 
 
 @app.route('/items/<string:identifier>', methods=['GET', 'PUT', 'DELETE'])
@@ -48,7 +67,7 @@ def device(identifier):
 
     elif request.method == 'PUT':
         try:
-            args = device_update_schema.load(request.json, partial=True)  # Allow partial updates
+            args = device_schema.load(request.json, partial=True)  # Allow partial updates
         except ValidationError as err:
             return jsonify(err.messages), 400
 
@@ -74,7 +93,7 @@ def device_inventory():
 
     elif request.method == 'POST':
         try:
-            args = device_post_schema.load(request.json)
+            args = device_schema.load(request.json)
         except ValidationError as err:
             return jsonify(err.messages), 400
         new_device = dal.post(args)
