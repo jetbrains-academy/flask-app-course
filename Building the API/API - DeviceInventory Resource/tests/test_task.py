c90cb2b9280e88f06a6c70d087814a2f3cf8d856
sofiiako <sofiia.kolchanova@gmail.com>
2024-02-06 20:07:32 +0100

    updated marshmallow schema in the DeviceInventory Resource

--- Building the API/API - DeviceInventory Resource/tests/test_task.py
+++ Building the API/API - DeviceInventory Resource/tests/test_task.py
@@ -1,4 +1,5 @@
 import flask_unittest
+import json
 
 from api import app
 
@@ -9,11 +10,17 @@ class TestClient(flask_unittest.ClientTestCase):
 
     def test_get_with_client(self, client):
         response = client.get('/items')
+        # Maybe we should reformat all such tests in this way =/
+        actual_response = json.loads(response.data.decode('utf-8'))
+        expected_response = {
+            "items": {
+                "001": {"id": "001", "location": "hall", "name": "Light bulb", "status": "off"},
+                "002": {"id": "002", "location": "bedroom", "name": "Humidity sensor", "status": "on"},
+                "003": {"id": "003", "location": "bedroom", "name": "Humidifier", "status": "off"}
+            }
+        }
         self.assertEqual(response.status_code, 200, msg=f"GET request resulted in an unexpected response code {response.status_code}.")
-        self.assertEqual(response.data, (b'{"items":{"001":{"id":"001","location":"hall","name":"Light bulb","status":"'
-                                         b'off"},"002":{"id":"002","location":"bedroom","name":"Humidity_sensor","statu'
-                                         b's":"on"},"003":{"id":"003","location":"bedroom","name":"Humidifier","status"'
-                                         b':"off"}}}\n'), msg="GET request resulted in unexpected response content.")
+        self.assertEqual(actual_response, expected_response, msg="GET request resulted in unexpected response content.")
 
     def test_post_with_client(self, client):
         response = client.post('/items', json={"id": "100500", "name": "TestDevice", "location": "somewhere", "status": "off"})
@@ -24,6 +31,11 @@ class TestClient(flask_unittest.ClientTestCase):
     def test_post_err_id_with_client(self, client):
         response = client.post('/items', json={"name": "TestDevice", "location": "somewhere", "status": "off"})
         self.assertEqual(response.status_code, 400, msg="Argument 'id' must be required")
+
+    def test_post_existing_id_with_client(self, client):
+        response = client.post('/items', json={"id": "001", "name": "TestDevice", "location": "somewhere", "status": "off"})
+        self.assertEqual(response.status_code, 400, msg=f"POST request resulted in an unexpected response code {response.status_code}.")
+
 
     def test_post_err_name_with_client(self, client):
         response = client.post('/items', json={"id": "100500", "location": "somewhere", "status": "off"})
